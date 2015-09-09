09428fc6b53a45dd8757e752ca151504c8cbe1e7
Adenike Adebiyi <adenikeadebiyi@icloud.com>
2015-09-08 21:25:00 -0500

    Assignment 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -53,5 +53,24 @@ class AddressBook
 
     return nil
   end
+
+  def iterative_search(name)
+
+    index = 0
+    upper = entries.length - 1
+
+    while index <= upper
+
+      if name == entries[index].name
+        return entries[index]
+      else
+        index += 1
+      end
+      
+    end
+
+    return nil
+
+  end
         
 end
